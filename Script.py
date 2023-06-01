44651b288437d25090ba72a1c528d8d92209a89d
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-06-01 15:46:42 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -7,10 +7,10 @@ class script(object):
 
 <b>✦ ɪᴛs ᴇᴀsʏ ᴛᴏ ᴜsᴇ ᴍᴇ. ᴊᴜsᴛ ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴀɴᴅ ᴍᴀᴋᴇ ᴍᴇ ᴀᴅᴍɪɴ</b>"""
 
-    MY_ABOUT_TXT = """<b>✯ Mʏ Nᴀᴍᴇ:  <a href=https://t.me/BotTest_MsBot>ᴀᴜᴛᴏ ꜰɪʟᴛᴇʀ ʙᴏᴛ</a></b>
+    MY_ABOUT_TXT = """<b>✯ ᴍᴀɪɴ ʙᴏᴛ:  <a href=https://t.me/BotTest_MsBot>ᴀᴜᴛᴏ ꜰɪʟᴛᴇʀ ʙᴏᴛ</a></b>
 <b>✯ Cʀᴇᴀᴛᴏʀ:  <a href=http://t.me/Mahi_458>ᴍᴀʜᴇꜱʜ.ꜱ</a></b>
-<b>✯ ᴍᴏᴠɪᴇꜱ: <a href=https://t.me/MsMovieRequests>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>
-<b>✯ ᴜᴩᴅᴀᴛᴇꜱ:  <a href=https://t.me/Cinema_Rockets>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>"""
+<b>✯ ᴍᴏᴠɪᴇꜱ: <a href=https://t.me/+fQW_IELD_mhiMGY9>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>
+<b>✯ ᴜᴩᴅᴀᴛᴇꜱ:  <a href=https://t.me/+fQW_IELD_mhiMGY9>ᴄʟɪᴄᴋ ʜᴇʀᴇ</a></b>"""
 
     MY_OWNER_TXT = """<b>★ Name: ᴍᴀʜᴇꜱʜ.ꜱ</b>
 <b>★ Username: @MaHi_458</b>
